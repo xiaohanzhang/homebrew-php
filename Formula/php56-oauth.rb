0f8af5d10888a58e8e27fe068b60f0acea28c5ae
Xiaohan Zhang <xiaohan.zhang.xz@gmail.com>
2019-09-10 11:55:07 -0400

    Update php56-oauth.rb

--- Formula/php56-oauth.rb
+++ Formula/php56-oauth.rb
@@ -1,6 +1,6 @@
 require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
-class PhpAT56Oauth < AbstractPhp56Extension
+class Php56Oauth < AbstractPhp56Extension
   init
   desc "OAuth 1.0 consumer and provider"
   homepage "https://pecl.php.net/package/oauth"
